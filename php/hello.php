eae46c0757ada75822eb727740ec1f66df44c02e
Rinju Pokhrel1 <rinjupokhrel91@gmail.com>
2026-01-27 20:40:52 +0545

    basic php learning

---
+++ php/hello.php
@@ -0,0 +1,18 @@
+<?php
+// $full_name ="Rinju Pokhrel";
+// echo "Hello " .$full_name;
+
+//addition 
+// $a=5;
+// $b=6;
+// $c=$a+$b;
+
+// echo "the sum of the $a and $b is $c." 
+
+$a= $_POST["n1"];
+$b=$_POST["n2"];
+$R=$a+$b;
+ echo "the sum of the $a and $b is $R." 
+
+
+?>
